10c8192d660a19d1d9ad4208d8e33861461a2148
Kirill Kamakin <k.kamakin@protonmail.ch>
2019-11-25 22:28:56 +0300

    Implement toString for models
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- guess/models.py
+++ guess/models.py
@@ -13,7 +13,13 @@ class Answer(models.Model):
     name = models.CharField(max_length=100)
     difficulty = enum.EnumField(Difficulty)
 
+    def __str__(self):
+        return '{} with difficulty {}'.format(self.name, self.difficulty)
+
 
 class Image(models.Model):
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return '{}, image is {}'.format(self.answer, self.image)
